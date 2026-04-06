3b4d90c395c0091f31847c5aa7d0392680cb1773
Lane <l_dewald3@mail.fhsu.edu>
2026-04-06 17:45:06 -0500

    created the main project and book app and defined the model

---
+++ books/models.py
@@ -0,0 +1,16 @@
+# Lane DeWald
+# INF601 - Advanced Programming in Python
+# Mini Project 1: Personal Book Tracker
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
